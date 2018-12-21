d371ff1cded410cb7087d21b9df31aabd1d5ab91
susu su <suzhaoqiang122@163.com>
2018-12-22 00:31:20 +0800

    添加建表语句以及索引列表查询

--- debugger/src/main/java/com/su/debugger/ui/test/app/TableInfoActivity.java
+++ debugger/src/main/java/com/su/debugger/ui/test/app/TableInfoActivity.java
@@ -8,6 +8,8 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
@@ -28,6 +30,7 @@ public class TableInfoActivity extends BaseAppCompatActivity {
     private String mTableName;
     private String mTableSql;
     private RecyclerViewAdapter mAdapter;
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -37,14 +40,14 @@ public class TableInfoActivity extends BaseAppCompatActivity {
         mTableSql = intent.getStringExtra("sql");
         mDatabaseName = intent.getStringExtra("database_name");
         mTableName = intent.getStringExtra("table_name");
-        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        mRecyclerView = findViewById(R.id.recycler_view);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-        recyclerView.setLayoutManager(linearLayoutManager);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
         PreferenceItemDecoration decoration = new PreferenceItemDecoration(this, 0, 0);
-        recyclerView.addItemDecoration(decoration);
+        mRecyclerView.addItemDecoration(decoration);
         mAdapter = new RecyclerViewAdapter();
-        recyclerView.setAdapter(mAdapter);
+        mRecyclerView.setAdapter(mAdapter);
     }
 
     @Override
@@ -56,7 +59,18 @@ public class TableInfoActivity extends BaseAppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        resetMenu();
+        View header = makeHeaderView();
+        mAdapter.setHeaderView(header);
         loadData();
+    }
+
+    private View makeHeaderView() {
+        LayoutInflater inflater = getLayoutInflater();
+        View header = inflater.inflate(R.layout.debugger_header_table, mRecyclerView, false);
+        TextView sqlView = header.findViewById(R.id.sql);
+        sqlView.setText(mTableSql);
+        return header;
     }
 
     private void loadData() {
@@ -77,15 +91,98 @@ public class TableInfoActivity extends BaseAppCompatActivity {
             column.setNotNull(cursor.getInt(cursor.getColumnIndexOrThrow("notnull")) == 1);
             columns.add(column);
         } while (cursor.moveToNext());
+        dbInfoProvider.closeDb();
         mAdapter.updateData(columns);
     }
 
     public void info(@NonNull MenuItem item) {
+        DbInfoProvider dbInfoProvider = DbInfoProvider.getInstance(this, mDatabaseName);
+        List<Index> indexList = getIndexList(dbInfoProvider);
+        for (Index index : indexList) {
+            List<IndexInfo> indexInfoList = getIndexInfoList(dbInfoProvider, index);
+            index.indexInfoList.addAll(indexInfoList);
+        }
+        dbInfoProvider.closeDb();
+
         new AlertDialog.Builder(this)
-                .setTitle("建表语句")
-                .setMessage(mTableSql)
+                .setTitle("索引列表")
+                .setMessage(indexInfoString(indexList))
                 .setPositiveButton(R.string.confirm, null)
                 .show();
+    }
+
+    private String indexInfoString(@NonNull List<Index> indexList) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Index index : indexList) {
+            stringBuilder.append(index.name);
+            stringBuilder.append(" ");
+            if (index.unique) {
+                stringBuilder.append("unique");
+            }
+            stringBuilder.append("\n");
+            List<IndexInfo> indexInfoList = index.indexInfoList;
+            for (IndexInfo indexInfo : indexInfoList) {
+                stringBuilder.append(indexInfo.name);
+                stringBuilder.append(" ");
+                stringBuilder.append("(");
+                stringBuilder.append(indexInfo.seqno);
+                stringBuilder.append(", ");
+                stringBuilder.append(indexInfo.cid);
+                stringBuilder.append("), ");
+            }
+            stringBuilder.delete(stringBuilder.length() - 2, stringBuilder.length());
+            stringBuilder.append("\n");
+        }
+        if (!indexList.isEmpty()) {
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+        }
+        return stringBuilder.toString();
+    }
+
+    private List<IndexInfo> getIndexInfoList(@NonNull DbInfoProvider dbInfoProvider, @NonNull Index index) {
+        List<IndexInfo> list = new ArrayList<>();
+        Cursor cursor = dbInfoProvider.getIndexInfo(index.name);
+        if (cursor == null) {
+            return list;
+        }
+
+        cursor.moveToFirst();
+        do {
+            String name = cursor.getString(cursor.getColumnIndex("name"));
+            int cid = cursor.getInt(cursor.getColumnIndex("cid"));
+            int seqno = cursor.getInt(cursor.getColumnIndex("seqno"));
+            IndexInfo indexInfo = new IndexInfo(name, cid, seqno);
+            list.add(indexInfo);
+        } while (cursor.moveToNext());
+        cursor.close();
+        return list;
+    }
+
+    private List<Index> getIndexList(@NonNull DbInfoProvider dbInfoProvider) {
+        List<Index> indexList = new ArrayList<>();
+        Cursor cursor = dbInfoProvider.getTableIndexList(mTableName);
+        if (cursor == null) {
+            return indexList;
+        }
+        cursor.moveToFirst();
+        do {
+            String name = cursor.getString(cursor.getColumnIndex("name"));
+            int unique = cursor.getInt(cursor.getColumnIndex("unique"));
+            indexList.add(new Index(name, unique == 1));
+        } while (cursor.moveToNext());
+        cursor.close();
+        return indexList;
+    }
+
+    private void resetMenu() {
+        Menu menu = mToolbar.getMenu();
+        MenuItem info = menu.findItem(R.id.info);
+        DbInfoProvider dbInfoProvider = DbInfoProvider.getInstance(this, mDatabaseName);
+        Cursor cursor = dbInfoProvider.getTableIndexList(mTableName);
+        info.setVisible(cursor != null && cursor.getCount() > 0);
+        if (cursor != null) {
+            cursor.close();
+        }
     }
 
     @Override
@@ -117,6 +214,48 @@ public class TableInfoActivity extends BaseAppCompatActivity {
             typeView.setText("type: " + item.getType());
             pkView.setVisibility(item.isPk() ? View.VISIBLE : View.GONE);
             notnullView.setVisibility(item.isNotNull() ? View.VISIBLE : View.GONE);
+        }
+    }
+
+    private static class Index {
+        private String name;
+        private boolean unique;
+        private List<IndexInfo> indexInfoList = new ArrayList<>();
+
+        Index(String name, boolean unique) {
+            this.name = name;
+            this.unique = unique;
+        }
+
+        @NonNull
+        @Override
+        public String toString() {
+            return "Index{" +
+                    "name='" + name + '\'' +
+                    ", unique=" + unique +
+                    '}';
+        }
+    }
+
+    private static class IndexInfo {
+        private String name;
+        private int cid;
+        private int seqno;
+
+        IndexInfo(String name, int cid, int seqno) {
+            this.name = name;
+            this.cid = cid;
+            this.seqno = seqno;
+        }
+
+        @NonNull
+        @Override
+        public String toString() {
+            return "IndexInfo{" +
+                    "name='" + name + '\'' +
+                    ", cid=" + cid +
+                    ", seqno=" + seqno +
+                    '}';
         }
     }
 
