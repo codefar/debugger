6294d2ebd8cdd253f82fee26992be711df5c6b2f
susu su <suzhaoqiang122@163.com>
2019-01-03 23:46:52 +0800

    添加demo

---
+++ sample/src/main/java/com/su/sample/RequestActivity.java
@@ -0,0 +1,88 @@
+package com.su.sample;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONException;
+
+import java.io.IOException;
+
+import okhttp3.CacheControl;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * Created by su on 18-1-3.
+ */
+
+public class RequestActivity extends BaseAppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
+
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample_main);
+        TextView textView = findViewById(R.id.text);
+        textView.setText("开启数据模拟或者停机维护后，此页面数据返回结果则变为相应模拟数据");
+        mSwipeRefreshLayout = findViewById(R.id.swipe_refresh);
+        mSwipeRefreshLayout.setOnRefreshListener(this);
+        mSwipeRefreshLayout.post(() -> {
+            mSwipeRefreshLayout.setRefreshing(true);
+            onRefresh();
+        });
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        setTitle("网络请求测试");
+    }
+
+    private void test() {
+        Request.Builder builder = new Request.Builder()
+                .cacheControl(CacheControl.FORCE_NETWORK)
+                .url(Url.BANNER);
+        Request request = builder.build();
+        OkHttpClient client = RequestHelper.getClient();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                Toast.makeText(RequestActivity.this, "网络错误", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    String result = response.body().string();
+                    Object object;
+                    try {
+                        object = JSON.toJSONString(JSON.parse(result), true);
+                    } catch (JSONException e) {
+                        object = result;
+                    }
+                    String text = object == null ? "" : object.toString();
+                    runOnUiThread(() -> {
+                        mSwipeRefreshLayout.setRefreshing(false);
+                        ((TextView) findViewById(R.id.content)).setText(text);
+                    });
+                } else {
+                    Toast.makeText(RequestActivity.this, "请求错误", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRefresh() {
+        test();
+    }
+}
---
+++ sample/src/main/java/com/su/sample/SampleApplication.java
@@ -0,0 +1,55 @@
+package com.su.sample;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.su.debugger.Debugger;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by su on 18-1-2.
+ */
+
+public class SampleApplication extends Application {
+
+    private static final String TAG = SampleApplication.class.getSimpleName();
+    public static Context sContext;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sContext = this;
+        initDebugger(this);
+        initSharedPreference();
+    }
+
+    private static void initDebugger(Application application) {
+        if (BuildConfig.DEBUG) {
+            Debugger.init(application, "com.su.sample.MySupplier");
+        }
+    }
+
+    private void initSharedPreference() {
+        Set<String> set = new HashSet<>();
+        set.add("a");
+        set.add("b");
+        set.add("c");
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        sharedPreferences.edit()
+                .putInt("int_test", 1)
+                .putLong("long_test", 0x80000000)
+                .putFloat("float_test", 1.1f)
+                .putBoolean("boolean_test", true)
+                .putString("string_test", "string...")
+                .putStringSet("set_test", set)
+                .apply();
+    }
+
+    public static Context getContext() {
+        return sContext;
+    }
+}
